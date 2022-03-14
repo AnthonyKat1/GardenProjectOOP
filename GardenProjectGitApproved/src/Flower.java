b1d2ef2d12a64fde3f42093a57bf4d5aa94a6e89
persseth <75769740+persseth@users.noreply.github.com>
2022-03-13 22:48:57 -0700

    Update harvest day

--- GardenProjectGitApproved/src/Flower.java
+++ GardenProjectGitApproved/src/Flower.java
@@ -1,15 +1,22 @@
+package plant;
+
 public class Flower extends Plant {
-    public Flower(int x, int y, String name) {
-        // 3 water, 5 leaf, 15 tempHealth, 70-90 temp range, 15 days until harvest
-        super(3, 5, 15, 70, 90, 15, x, y, name);
-    }
+  public Flower(int x, int y, String name) {
+    // 3 water, 5 leaf, 15 tempHealth, 70-90 temp range, 15 days until harvest
+    super(3, 5, 15, 70, 90, 15, x, y, name, "Flower");
+  }
 
-    // flower get waters, and water resets at 3 again
-    public void water() {
-        super.setWaterHealth(3);
-    }
+  // flower get waters, and water resets at 3 again
+  public void water() {
+    super.setWaterHealth(3);
+  }
 
-    public String toString() {
-        return "F: " + super.toString();
-    }
+  // harvest to the flower and reset harvest day
+  public void harvest() {
+    super.setDayToHarvest(15);
+  }
+
+  public String toString() {
+    return "F: " + super.toString();
+  }
 }
